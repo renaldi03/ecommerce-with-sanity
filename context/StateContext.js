62ef6499dcdf542a31bdca615b5370df6b071aed
renaldia63@gmail.com <renaldia63@gmail.com>
2022-05-26 09:41:09 +0700

    upload

--- context/StateContext.js
+++ context/StateContext.js
@@ -92,7 +92,10 @@ export const StateContext = ({ children }) => {
             decQty, 
             onAdd,
             toggleCartItemQuanitity,
-            onRemove
+            onRemove,
+            setCartItems,
+            setTotalPrice,
+            setTotalQuantities
         }}>
 
             {children}
